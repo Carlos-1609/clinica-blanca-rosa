0077a9b330e6f9fd1ee520e825815edaf41f3974
Carlos Ordoñez <carlosord1609@gmail.com>
2023-01-03 15:00:22 -0600

    assets 2

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
     react(),
     VitePWA({
       includeAssets: ["favicon.svg", "favicon.ico", "assets/*"],
+      manifest: {
+        name: "App name",
+        short_name: "App short name",
+        description: "App description",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
     }),
   ],
 });
